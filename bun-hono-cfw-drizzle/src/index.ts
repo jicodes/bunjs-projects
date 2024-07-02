573129a7c3811e505a943464a473141607a338ca
jicodes <liu.ji1@northeastern.edu>
2024-07-02 13:52:10 -0700

    add endpoints to server

--- bun-hono-cfw-drizzle/src/index.ts
+++ bun-hono-cfw-drizzle/src/index.ts
@@ -1,4 +1,7 @@
+import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
+import { posts } from "./db/schema";
+import { db } from "../../bun-hono-drizzle-htmx/db/index";
 
 export type Env = {
     DB: D1Database;
@@ -6,8 +9,18 @@ export type Env = {
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/", (c) => {
-    return c.text(`Hello Hono!`);
+app.get("/posts", async (c) => {
+    const db = drizzle(c.env.DB);
+    const result = await db.select().from(posts).all();
+    return c.json(result);
+}).post("/posts", async (c) => {
+    const db = drizzle(c.env.DB);
+    const { title, content } = await c.req.json();
+    const result = await db
+        .insert(posts)
+        .values({ title, content })
+        .returning();
+    return c.json(result);
 });
 
 export default app;
